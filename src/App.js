7dd2f5617eec58b8a9545189942937875d094933
notthesinger <james.edmund.t@gmail.com>
2022-10-30 09:21:00 -0700

    added back ground image and nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import background from './img/background.jpg'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ 
+        backgroundImage: `url(${background})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width: '100vw',
+        height: '100vh'
+
+      }}>
+        <HomePage/>
+      </div>
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      
+    </>
   );
 }
 
