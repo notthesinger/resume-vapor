1b762938205373f83d26c55079c3a0f5ecf788f5
notthesinger <james.edmund.t@gmail.com>
2022-12-20 18:06:20 -0700

    added nav bar

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,20 +1,12 @@
 import React from "react";
-import {Container, Nav, Navbar} from 'react-bootstrap';
+import NavBarHome from '../Components/NavBarHome'
+
 
 const HomePage = () => {
 
     return(
         <>
-             <Navbar bg="dark" variant="dark">
-                <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-                </Container>
-            </Navbar>
+            <NavBarHome />
         </>
     )
 
