e5586e33569c9667dedae53e6c11e61b4b0423f6
James Taylor <james.edmund.t@gmail.com>
2023-11-24 22:08:41 -0700

    got a cool color cheme and made the background color fill the whole screen

---
+++ src/components/navbar/NavBarVapor.tsx
@@ -0,0 +1,10 @@
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+export default function NavBarVapor (){
+    return (
+        <Navbar bg="dark" data-bs-theme="dark">
+            <Navbar.Brand href="#home">James Taylor</Navbar.Brand>
+        </Navbar>
+    )
+}
